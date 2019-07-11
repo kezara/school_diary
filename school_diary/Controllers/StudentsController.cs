2fc9873c17367d38726738d4b6020fb51240e963
Borisav Ignjatov <borisav.ignjatov@gmail.com>
2019-07-11 21:34:15 +0200

    minor changes to students and subjects controllers

--- school_diary/Controllers/StudentsController.cs
+++ school_diary/Controllers/StudentsController.cs
@@ -12,6 +12,7 @@ using System.Web.Http.Description;
 
 namespace school_diary.Controllers
 {
+    [RoutePrefix("api/students")]
     public class StudentsController : ApiController
     {
         private static Logger logger = LogManager.GetCurrentClassLogger();
